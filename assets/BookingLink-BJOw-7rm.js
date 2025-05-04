e2e40314354bb6e707db087ddc8b0142e20eff8e
Alexander Komarov <sanrkom@gmail.com>
2025-05-04 14:00:29 +0300

    Updates

--- assets/BookingLink-BJOw-7rm.js
+++
@@ -1,1 +0,0 @@
-import{j as n,l as r}from"./index-Bwu_nYHg.js";import{L as i}from"./LinkMemo-4S_5ug9b.js";const e=({id:o})=>o?n.jsx(i,{href:r.bookings.edit({id:o.toString()}),children:o}):null;export{e as B};
---
+++ assets/useGetProfile-g1cOOM3x.js
@@ -0,0 +1,1 @@
+import{u as e}from"./AppLoading-Ykt0l9tN.js";const o=()=>e["/profile"]();export{o as u};
