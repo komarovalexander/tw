c80be794d7a207efb7ddf0bd8d430e8ef34b5b28
Alexander Komarov <sanrkom@gmail.com>
2025-05-04 13:18:07 +0300

    Updates

--- assets/useGetProfile-BCJocYMg.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./AppLoading-BdA6FHVt.js";const o=()=>e["/profile"]();export{o as u};
---
+++ assets/useGetProfile-CULDhplD.js
@@ -0,0 +1,1 @@
+import{u as e}from"./AppLoading-COaGQoJo.js";const o=()=>e["/profile"]();export{o as u};
