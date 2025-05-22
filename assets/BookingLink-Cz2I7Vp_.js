37a1170ecdb6807a60a4c49d503a20aec7fe2b2f
Alexander Komarov <sanrkom@gmail.com>
2025-05-22 22:08:53 +0300

    Updates

---
+++ assets/BookingLink-Cz2I7Vp_.js
@@ -0,0 +1,1 @@
+import{j as n,l as r}from"./index-BHAJFkS7.js";import{L as i}from"./LinkMemo-BDoxtYqj.js";const e=({id:o})=>o?n.jsx(i,{href:r.bookings.edit({id:o.toString()}),children:o}):null;export{e as B};
