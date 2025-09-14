c698593e992a9fca2946e552c1f56a48e29de2be
Alexander Komarov <sanrkom@gmail.com>
2025-09-14 20:53:08 +0300

    Updates

--- assets/PlusOutlined-hM5DK3jI.js
+++
@@ -1,1 +0,0 @@
-import{I as o,_ as n}from"./AntdIcon-CjrMuquu.js";import{r}from"./index-DNKWA5Yr.js";import{P as a}from"./PlusOutlined-ByZyTB3m.js";var s=function(e,t){return r.createElement(o,n({},e,{ref:t,icon:a}))},m=r.forwardRef(s);export{m as R};
